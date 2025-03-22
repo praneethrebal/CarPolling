cc6400c5ed708ac7468e18142c1e8c393d03081d
praneethrebal <113005764+praneethrebal@users.noreply.github.com>
2025-03-22 10:16:20 +0530

    working with version2

--- car_polling-ui/src/App.js
+++ car_polling-ui/src/App.js
@@ -1,39 +1,9 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Login from "./Compoments/Login";
-import { Authpro } from "./Compoments/Auth/AuthContext";
-import Passeanger from "./Compoments/Passeanger";
-import Driver from "./Compoments/Driver";
-import IsAuth from "./Compoments/Auth/IsAuth";
 
 function App() {
   return (
     <div>
-      <Authpro>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Login />} />
-
-            <Route
-              path="/passeange"
-              element={
-                <IsAuth>
-                  <Passeanger />
-                </IsAuth>
-              }
-            />
-
-            <Route
-              path="/driver"
-              element={
-                <IsAuth>
-                  <Driver />
-                </IsAuth>
-              }
-            />
-          </Routes>
-        </BrowserRouter>
-      </Authpro>
+      <h1>welcome</h1>
     </div>
   );
 }
